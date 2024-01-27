d029441263b68da2937fb90c8572483df583793e
Qusai GAZAWY <92310650+cando-jo@users.noreply.github.com>
2024-01-27 21:26:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import random
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='$', intents=intents)
+bot = commands.Bot(command_prefix='/', intents=intents)
 
 @bot.event
 async def on_ready():
